fa525f43ad06dfc7880fceea2c61fcd50353988c
Debug0-0 <49203045@students.mq.edu.au>
2025-05-16 15:00:15 +1000

    Add property

--- Assets/Tiled/Tilemap/TinyTown_Tilesheet.tsx
+++ Assets/Tiled/Tilemap/TinyTown_Tilesheet.tsx
@@ -1,39 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TinyTown_Tilesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
  <image source="Tiled/Tilemap/tilemap.png" width="203" height="186"/>
- <tile id="7">
+ <tile id="15">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="2" y="7" width="12" height="9"/>
+   <object id="1" x="0" y="0" width="15" height="15"/>
   </objectgroup>
  </tile>
- <tile id="18">
+ <tile id="16">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="10" y="0" width="6" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="19">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="20">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="6" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="31">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="2" y="0" width="12" height="7"/>
+   <object id="1" x="0" y="0" width="15" height="15"/>
   </objectgroup>
  </tile>
  <tile id="44">
   <objectgroup draworder="index" id="2">
    <object id="1" x="4" y="2" width="12" height="14"/>
+   <object id="2" x="-73" y="-686"/>
+   <object id="3" x="8.375" y="8.125" width="1.875"/>
   </objectgroup>
  </tile>
  <tile id="45">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="2" width="16" height="12"/>
+   <object id="1" x="0.125" y="1.875" width="15.75" height="12.625"/>
   </objectgroup>
  </tile>
  <tile id="46">
@@ -44,21 +31,25 @@
  <tile id="47">
   <objectgroup draworder="index" id="2">
    <object id="1" x="4" y="2" width="8" height="14"/>
+   <object id="2" x="4" y="2" width="8" height="14"/>
+   <object id="3" x="4" y="2" width="8" height="14"/>
+   <object id="4" x="4" y="2" width="8" height="14"/>
+   <object id="5" x="4" y="2" width="8" height="14"/>
+   <object id="6" x="4" y="2" width="8" height="14"/>
+   <object id="7" x="4" y="2" width="8" height="14"/>
+   <object id="11" x="4" y="2" width="8" height="14"/>
+   <object id="12" x="4" y="2" width="8" height="14"/>
   </objectgroup>
  </tile>
  <tile id="56">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="4" y="0" width="8" height="16"/>
-  </objectgroup>
- </tile>
- <tile id="57">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="16" height="15"/>
+   <object id="1" x="10" y="7.375" width="2.625"/>
+   <object id="2" x="4" y="0" width="8" height="16"/>
   </objectgroup>
  </tile>
  <tile id="58">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="4" y="0" width="8" height="16"/>
+   <object id="1" x="3.125" y="-0.125" width="10.125" height="16.25"/>
   </objectgroup>
  </tile>
  <tile id="59">
@@ -68,7 +59,11 @@
  </tile>
  <tile id="68">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="4" y="0" width="12" height="14"/>
+   <object id="1" x="11.5" y="7.125"/>
+   <object id="2" x="4" y="0" width="12" height="14"/>
+   <object id="3" x="4" y="0" width="12" height="14"/>
+   <object id="4" x="4" y="0" width="12" height="14"/>
+   <object id="5" x="4" y="0" width="12" height="14"/>
   </objectgroup>
  </tile>
  <tile id="69">
@@ -84,6 +79,8 @@
  <tile id="71">
   <objectgroup draworder="index" id="2">
    <object id="1" x="4" y="0" width="8" height="14"/>
+   <object id="2" x="4" y="0" width="8" height="14"/>
+   <object id="3" x="4" y="0" width="8" height="14"/>
   </objectgroup>
  </tile>
  <tile id="80">
@@ -106,15 +103,31 @@
    <object id="1" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
+ <tile id="91">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="95">
   <objectgroup draworder="index" id="2">
    <object id="1" x="2" y="2" width="12" height="14"/>
-   <object id="2" x="2" y="2" width="12" height="14">
+   <object id="2" x="2" y="2" width="12" height="14"/>
+   <object id="3" x="2" y="2" width="12" height="14">
     <properties>
-     <property name="unity:Iayer" value="true"/>
-     <property name="unity:IsTrigger" value="Hazard"/>
+     <property name="unity:IsTrigger" value="true"/>
+     <property name="unity:layer" value="Hazard"/>
     </properties>
    </object>
+  </objectgroup>
+ </tile>
+ <tile id="130">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="131">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
 </tileset>
